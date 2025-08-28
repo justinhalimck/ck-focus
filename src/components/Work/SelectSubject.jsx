b304e3ff613869a86fabb1067693b4cc3fb5ce8f
Luc Street <216481438+lucas-colorkrew@users.noreply.github.com>
2025-08-28 17:14:36 +0900

    feat: autocomplete select subject

--- src/components/Work/SelectSubject.jsx
+++ src/components/Work/SelectSubject.jsx
@@ -1,34 +1,28 @@
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
-import { useId } from "react";
+import { Autocomplete, TextField } from "@mui/material";
+
+const SUBJECTS = {
+  "00000": "Example Project 0",
+  "00001": "Example Project 1",
+  "00002": "Example Project 2",
+  "00003": "Example Project 3",
+};
 
 const SelectSubject = ({ onSelect }) => {
-  const labelId = useId();
-
-  const SUBJECTS = {
-    "00000": "Example Project 0",
-    "00001": "Example Project 1",
-    "00002": "Example Project 2",
-    "00003": "Example Project 3",
-  };
-
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-        <InputLabel id={labelId}>Select subject</InputLabel>
-        <Select
-          labelId={labelId}
-          label="Select subject"
-          onChange={(e) => onSelect(e.target.value, SUBJECTS[e.target.value])}
-        >
-          {Object.entries(SUBJECTS).map(([key, value]) => {
-            return (
-              <MenuItem key={key} value={key}>
-                {value}
-              </MenuItem>
-            );
-          })}
-        </Select>
-      </FormControl>
+      <Autocomplete
+        disableClearable
+        options={Object.entries(SUBJECTS).map(([k, v]) => ({
+          label: v,
+          code: k,
+        }))}
+        sx={{ margin: "2em", marginTop: "5vh", textAlign: "left" }}
+        renderInput={(params) => (
+          <TextField {...params} label="Select subject" />
+        )}
+        onChange={(_e, { code }) => onSelect(code, SUBJECTS[code])}
+      />
+
       <h1 style={{ marginTop: "20vh" }}>Please select your subject.</h1>
     </div>
   );

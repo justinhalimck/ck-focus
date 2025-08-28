3a95019300e88f24b3de34e3779f7014a89551ad
Luc Street <216481438+lucas-colorkrew@users.noreply.github.com>
2025-08-28 15:56:57 +0900

    feat: add separate dir for pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import { CssBaseline } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.jsx";
 import CountdownTimer from "./components/CountdownTimer.jsx";
-import Start from "./components/Start.jsx";
+import Home from "./pages/Home.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <CssBaseline />
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Start />} />
+        <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<App />} />
         <Route
           path="/timer"

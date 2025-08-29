558c0f1e683dcdcb64710c6e035638dd389ef7ef
Nikita Borovikov <nikita.borovikov@CK0625-001.local>
2025-08-29 13:13:30 +0900

    new trees

--- src/components/Work/ProjectProgress.jsx
+++ src/components/Work/ProjectProgress.jsx
@@ -3,12 +3,7 @@ import tree_stage_1 from "../../assets/tree_stage_1.svg";
 import tree_stage_2 from "../../assets/tree_stage_2.svg";
 import tree_stage_3 from "../../assets/tree_stage_3.svg";
 import tree_stage_4 from "../../assets/tree_stage_4.svg";
-const SUBJECTS = {
-  "00000": "Example Project 0",
-  "00001": "Example Project 1",
-  "00002": "Example Project 2",
-  "00003": "Example Project 3",
-};
+import { SUBJECTS } from "../../utils/subjects";
 
 const ProjectProgress = ({ code, subject }) => {
   return (
@@ -37,10 +32,14 @@ const ProjectProgress = ({ code, subject }) => {
                 background: "white",
                 boxShadow: "0 4px 4px 0 rgba(0, 0, 0, 0.10)",
             }}>
-                {code === "00000" && (<img src={tree_stage_1} alt="tree" style={{height: "20vw", marginTop: "52vw"}}/>)}
-                {code === "00001" && (<img src={tree_stage_2} alt="tree" style={{height: "35vw", marginTop: "37vw"}}/>)}
-                {code === "00002" && (<img src={tree_stage_3} alt="tree" style={{height: "50vw", marginTop: "22vw"}}/>)}
-                {code === "00003" && (<img src={tree_stage_4} alt="tree" style={{height: "65vw", marginTop: "7vw"}}/>)}
+                {code === "933251" && (<img src={tree_stage_1} alt="tree" style={{height: "20vw", marginTop: "52vw"}}/>)}
+                {code === "933264" && (<img src={tree_stage_2} alt="tree" style={{height: "35vw", marginTop: "37vw"}}/>)}
+                {code === "933255" && (<img src={tree_stage_3} alt="tree" style={{height: "50vw", marginTop: "22vw"}}/>)}
+                {code === "902294" && (<img src={tree_stage_4} alt="tree" style={{height: "65vw", marginTop: "7vw"}}/>)}
+                {code === "953277" && (<img src={tree_stage_3} alt="tree" style={{height: "50vw", marginTop: "22vw"}}/>)}
+                {code === "943215" && (<img src={tree_stage_2} alt="tree" style={{height: "35vw", marginTop: "37vw"}}/>)}
+                {code === "923303" && (<img src={tree_stage_3} alt="tree" style={{height: "50vw", marginTop: "22vw"}}/>)}
+                {code === "943226" && (<img src={tree_stage_3} alt="tree" style={{height: "50vw", marginTop: "22vw"}}/>)}
             </div>
         
         </div>
